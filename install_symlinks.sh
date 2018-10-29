7fc2a563a0a1e1536590a741544d936537a336df
Peder Bergebakken Sundt <pbsds@hotmail.com>
2018-10-29 03:29:17 +0100

    Add application shortcuts

--- install_symlinks.sh
+++ install_symlinks.sh
@@ -3,8 +3,13 @@
 DIR=$(pwd)
 #DIR=$(git rev-parse --show-toplevel)
 
-ln -vsf $DIR/config/dot_profile $HOME/.profile
-ln -vsf $DIR/config/dot_atom/config.cson $HOME/.atom/config.cson
-ln -vsf $DIR/config/dot_atom/keymap.cson $HOME/.atom/keymap.cson
-ln -vsf $DIR/config/micro/bindings.json $HOME/.config/micro/bindings.json
-ln -vsf $DIR/config/micro/settings.json $HOME/.config/micro/settings.json  
+ln -vsf "$DIR/config/dot_profile" "$HOME/.profile"
+ln -vsf "$DIR/config/dot_atom/config.cson" "$HOME/.atom/config.cson"
+ln -vsf "$DIR/config/dot_atom/keymap.cson" "$HOME/.atom/keymap.cson"
+ln -vsf "$DIR/config/micro/bindings.json" "$HOME/.config/micro/bindings.json"
+ln -vsf "$DIR/config/micro/settings.json" "$HOME/.config/micro/settings.json  "
+
+find "$DIR/config/applications" -type f |
+while read file; do
+    ln -vsf "$file" "$HOME/.local/share/applications/$(basename "$file")"
+done
