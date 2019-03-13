b315a343a338088102bd66a77d4622958781c9fa
Peder Bergebakken Sundt <pbsds@hotmail.com>
2019-03-13 14:00:52 +0100

    Expand the quick python interpreter setup script

--- opt/python-interactive-imports.py
+++ opt/python-interactive-imports.py
@@ -8,13 +8,26 @@ import numpy as np
 from statistics import *
 
 def plot(x, *args):
-    import matplotlib.pyplot as plt
-    for i in args:
-        plt.plot(x[:len(i)], i)
-    plt.show()
+	import matplotlib.pyplot as plt
+	for i in args:
+		plt.plot(x[:len(i)], i)
+	plt.show()
+
+
+def hist(x, bins=20):
+	import matplotlib.pyplot as plt
+	plt.hist(x, bins)
+	plt.show()
+
+def scatter(x, *args):
+	import matplotlib.pyplot as plt
+	for i in args:
+		plt.plot(x[:len(i)], i, ".")
+	plt.show()
+
 
 def rad(degrees):
-    return degrees * pi / 180
+	return degrees * pi / 180
 
 def deg(radians):
-    return radians / pi * 180
+	return radians / pi * 180
