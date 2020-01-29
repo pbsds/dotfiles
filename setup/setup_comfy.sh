d56af0573bcc72ac814dbd19a0f8190f704dcd92
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-01-29 19:17:13 +0100

    todos

---
+++ setup/setup_comfy.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+DIR=$(git rev-parse --show-toplevel)
+
+# download backgrounds
+(
+    mkdir -p ~/Pictures/backgrounds_randomized
+    cd ~/Pictures/backgrounds_randomized
+    curl "http://pvv.ntnu.no/~pederbs/backgrounds.txt" |
+    ( while read line; do
+        (
+        wget -q "$line"
+        echo "$line"
+        ) &
+    done
+    wait )
+)
+
+# set random bg
+(
+    file="$(ls ~/Pictures/backgrounds_randomized -1 | shuf -n 1)"
+    (
+        echo "[org/gnome/desktop/background]"
+        echo "picture-uri='file://$HOME/Pictures/backgrounds_randomized/$file'"
+        echo "picture-options='zoom'"
+    ) | dconf load /
+)
+
+#todos:
+# - eog
+# - mpv
+# - last.fm
+# - spotify
+# - automounts?
+
+
+sudo bash -c ":" || exit 1
+function install {
+	if ! pacman -Qi $1 > /dev/null; then
+		sudo pacman --noconfirm -S $1
+	fi
+}
+
+# install x
