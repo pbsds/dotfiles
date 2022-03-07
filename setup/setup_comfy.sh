3b0a91229749140a9ed2dd702eb6041bd0d9f3a1
Peder Bergebakken Sundt <pbsds@hotmail.com>
2022-03-07 14:21:01 +0100

    stuff from settup up new computer

--- setup/setup_comfy.sh
+++ setup/setup_comfy.sh
@@ -48,6 +48,7 @@ cat <<EOF | xargs sudo pacman -S --noconfirm
     sshuttle
     discord
     transmission-gtk
+    scrcpy
 EOF
 
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
